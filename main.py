a9f970803a8111e422fbc668be38dcb05fe8f017
Doug Bogle <dbogle@bowdoin.edu>
2024-07-03 19:20:45 +0000

    autodeploying

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, url_for, flash, redirect, request
 from forms import RegistrationForm
 from flask_behind_proxy import FlaskBehindProxy
-import git
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 proxied = FlaskBehindProxy(app)
