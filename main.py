fb77672dbdbdbc7d889c216e305daedbb10d3ab5
Doug Bogle <dbogle@bowdoin.edu>
2024-07-03 21:49:51 +0000

    working on layout and storing response in db

--- main.py
+++ main.py
@@ -7,6 +7,22 @@ app = Flask(__name__)
 proxied = FlaskBehindProxy(app)
 app.config['SECRET_KEY'] = '1df9ffd8437d42b30dbf2042fa913737'                # this gets the name of the file so Flask knows it's name
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(20), unique=True, nullable=False)
+  email = db.Column(db.String(120), unique=True, nullable=False)
+  password = db.Column(db.String(60), nullable=False)
+
+  def __repr__(self):
+    return f"User('{self.username}', '{self.email}')"
+
+with app.app_context():
+  db.create_all()
+
+
 @app.route("/")      
 @app.route("/home")                       # this tells you the URL the method below is related to
 def home():
@@ -15,21 +31,28 @@ def home():
     Provide you with tags for your video, auto comment-liking, and an auto response feature."
     return render_template('home.html', subtitle='Home Page', text=home_message)       # this prints HTML to the webpage
 
+
 @app.route("/titles")
 def titles():
   return render_template('titles.html', subtitle='Title page', text='This is the title tool')
+
 
 @app.route("/comments")
 def comments():
   return render_template('comments.html', subtitle='Comment tool', text='This is the comment tool')
 
+
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit(): # checks if entries are valid
-        flash(f'Account created for {form.username.data}!', 'success')
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        # flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home')) # if so - send to home page
     return render_template('register.html', title='Register', form=form)
+
 
 @app.route("/update_server", methods=['POST'])
 def webhook():
